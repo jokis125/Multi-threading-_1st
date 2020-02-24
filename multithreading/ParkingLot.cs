a98550f16b7e7d905202a39859855aeabe3d9817
Jokūbas Kondrackas <jokubask@unity3d.com>
2020-02-24 22:41:15 +0200

    hopefully we good

---
+++ multithreading/ParkingLot.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class ParkingLot
+    {
+        private Dictionary<int, string> ParkingSpots = new Dictionary<int, string>();
+        private int currentId = 0;
+        private int lotSize;
+        public int CarCount;
+
+        public ParkingLot(int size)
+        {
+            lotSize = size;
+            for (var i = 0; i < size; i++)
+            {
+                ParkingSpots.Add(i, "");
+            }
+        }
+
+        private void AssignParkingSpace(string carNo, bool safe)
+        {
+            if (safe)
+            {
+                CarCount++;
+                lock (ParkingSpots)
+                {
+                    if (lotSize >= currentId + 1)
+                        ParkingSpots[currentId++] = carNo;
+                }
+            }
+            else
+            {
+                CarCount++;
+                if (lotSize >= currentId + 1)
+                    ParkingSpots[currentId++] = carNo;
+            }
+        }
+        
+        public void AddRandomCar()
+        {
+            //Thread.Sleep((lotSize/(CarCount+1))/100);
+            AssignParkingSpace($"{Program.RandomString(3)}{Program._random.Next(100, 999)}", false);
+        }
+        
+        public void AddRandomCarSafe()
+        {
+            //Thread.Sleep((lotSize/(CarCount+1))/100);
+            AssignParkingSpace($"{Program.RandomString(3)}{Program._random.Next(100, 999)}", true);
+        }
+
+        public void CheckForRaceCondition()
+        {
+            var temp = "Race condition happened because: ";
+            if (CarCount == lotSize && ParkingSpots.Count == lotSize)
+                temp = "Race condition didn't happen because: ";
+            Console.WriteLine(temp + $"CarCount = {CarCount}; DictSize = {ParkingSpots.Count}; LotSize = {lotSize}");
+        }
+
+        public void FillParkingSpots(bool safe)
+        {
+            
+            if (safe)
+            {
+                while (true)
+                {
+                    lock (ParkingSpots)
+                    {
+                        if (CarCount < lotSize)
+                        {
+                            ParkingSpots[currentId++] = $"{Program.RandomString(3)}{Program._random.Next(100, 999)}";
+                            CarCount++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
+                return;
+            }
+            while (true)
+            {
+                
+                if (CarCount < lotSize)
+                {
+                    ParkingSpots[currentId++] = $"{Program.RandomString(3)}{Program._random.Next(100, 999)}";
+                    CarCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void FreeParkingSpace(string carNo)
+        {
+            CarCount--;
+            for (var i = 0; i < ParkingSpots.Count; i++)
+            {
+                if (carNo == ParkingSpots[i])
+                    ParkingSpots[i] = "";
+            }
+        }
+
+        public void RemoveCarById(int id)
+        {
+            CarCount--;
+            ParkingSpots[id] = "";
+        }
+
+        public void RemoveLastCar()
+        {
+            CarCount--;
+            ParkingSpots[--currentId] = "";
+        }
+
+        public void FillParkingSpace()
+        {
+            for (var i = 0; i < ParkingSpots.Count; i++)
+            {
+                CarCount++;
+                ParkingSpots[i] = $"{Program.RandomString(3)}{Program._random.Next(100, 999)}";
+                currentId++;
+            }
+        }
+
+        public void PrintParkingSpace()
+        {
+            foreach (var keyValuePair in ParkingSpots.ToList())
+            {
+                Console.WriteLine(keyValuePair);
+            }
+        }
+    }
+}
---
+++ multithreading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static Random _random = new Random();
+        static void Main(string[] args)
+        {
+            var size = 10000;
+            var carpark = new ParkingLot(size);
+            
+            //carpark.FillParkingSpace();
+            
+            //for(var i = 0; i < size/2; i++)
+                //carpark.RemoveCarById(_random.Next(0, size/2));
+
+                /*for (var i = 0; i < size; i++)
+                {
+                    threadList.Add(new Thread(carpark.AddRandomCarSafe));
+                }*/
+                
+            List<Thread> threadList = new List<Thread>();
+            for (var i = 0; i < 16; i++)
+            {
+                threadList.Add(new Thread(()=> carpark.FillParkingSpots(true)));
+            }
+
+            foreach (var thread in threadList)
+            {
+                thread.Start();
+                //thread.Join();
+            }
+
+            foreach (var thread in threadList)
+            {
+                thread.Join();
+            }
+            carpark.PrintParkingSpace();
+            carpark.CheckForRaceCondition();
+
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
